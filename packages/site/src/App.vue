4efb0e4c61b4d4770b4d5f90dc295f6d59186895
aaronlamz <aaronlamz2022@gmail.com>
2024-01-17 21:51:43 +0800

    chore: add md loader

--- packages/site/src/App.vue
+++ packages/site/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Index from './components/index.vue'
+import homeen  from 'xsmart-ui/docs/markdown/home.en-US.md'
 </script>
 
 <template>
   <Index/>
+  <homeen/>
 </template>
 
 <style scoped>
