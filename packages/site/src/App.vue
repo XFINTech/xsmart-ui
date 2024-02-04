a07f5fa8d0ace88dd5000fcb2653f1c4d56d6502
aaronlamz <aaronlamz2022@gmail.com>
2024-02-04 17:25:27 +0800

    fix version & parse md

--- packages/site/src/App.vue
+++ packages/site/src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import Index from './components/index.vue';
-import homeen from 'xsmart-ui/docs/markdown/home.en-US.md';
-import homecn from 'xsmart-ui/docs/markdown/home.zh-CN.md';
+// import homeen from 'xsmart-ui/docs/markdown/home.en-US.md';
+// import homecn from 'xsmart-ui/docs/markdown/home.zh-CN.md';
+import rm from '../README.md';
 </script>
 
 <template>
   <Index />
-  <homeen />
-  <homecn />
+  <!-- <homeen /> -->
+  <!-- <homecn /> -->
+  <rm />
 </template>
 
 <style scoped></style>
--- packages/site/vite.config.js
+++ packages/site/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import Markdown from 'vite-plugin-md';
+import Markdown from 'unplugin-vue-markdown/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
