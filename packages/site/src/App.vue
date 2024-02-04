ed02c853be0176153cc481eb03d03d5de0dfd912
aaronlamz <aaronlamz2022@gmail.com>
2024-02-04 17:36:19 +0800

    fix md workspace dir error

--- packages/site/src/App.vue
+++ packages/site/src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
 import Index from './components/index.vue';
-// import homeen from 'xsmart-ui/docs/markdown/home.en-US.md';
-// import homecn from 'xsmart-ui/docs/markdown/home.zh-CN.md';
-import rm from '../README.md';
+import homeen from 'xsmart-ui/docs/guide/home.en-US.md';
+import homecn from 'xsmart-ui/docs/guide/home.zh-CN.md';
 </script>
 
 <template>
   <Index />
-  <!-- <homeen /> -->
-  <!-- <homecn /> -->
-  <rm />
+  <homeen />
+  <homecn />
 </template>
 
 <style scoped></style>
