413eb1b1d7413deaafa75dca5c1d88a7451028a8
aaronlamz <aaronlamz2022@gmail.com>
2024-02-05 12:21:47 +0800

    add stylelint &vscode format config

--- packages/site/src/App.vue
+++ packages/site/src/App.vue
@@ -5,7 +5,6 @@ import homecn from 'xsmart-ui/docs/guide/home.zh-CN.md';
 </script>
 
 <template>
-  <img alt="Vue logo" src="/logo.svg" width="500" height="500" />
   <Index />
   <homeen />
   <homecn />
