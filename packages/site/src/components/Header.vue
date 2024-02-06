d5f32b7b26ec175a17dd109c46ee00e2ebc6b64d
aaronlamz <aaronlamz2022@gmail.com>
2024-02-06 17:43:01 +0800

    style:fix

--- packages/site/src/components/Header.vue
+++ packages/site/src/components/Header.vue
@@ -16,9 +16,32 @@
       </div>
     </div>
     <div class="xsmart-doc-header-action">
-      <div class="action-item action-lang"></div>
-      <div class="action-item action-dark"></div>
-      <div class="action-item action-link"></div>
+      <div class="action-item doc-header-action__lang">
+        <div class="doc-dropdown__child">
+          <!-- <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              focusable="false"
+              viewBox="0 0 24 24"
+              class="option-icon"
+              data-v-764effdf=""
+            >
+              <path d="M0 0h24v24H0z" fill="none"></path>
+              <path
+                d=" M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z "
+                class="css-c4d79v"
+              ></path>
+            </svg>
+          </div> -->
+          <div class="doc-dropdown__child_content">
+            <div class="sublink">🇨🇳 中文</div>
+            <div class="sublink">🇺🇸 English</div>
+          </div>
+        </div>
+      </div>
+      <div class="action-item action-item__dark"></div>
+      <div class="action-item action-item__link"></div>
     </div>
   </div>
 </template>
@@ -34,6 +57,7 @@
   align-items: center;
   justify-content: space-between;
   height: 60px;
+  box-sizing: border-box;
   padding: 0 20px;
   backdrop-filter: blur(10px);
   background-color: rgba(255, 255, 255, 0.5);
